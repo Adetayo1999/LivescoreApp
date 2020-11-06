34d9243a30ac423f1bf9f1fcda205bbd53930b7e
Adetayo Omotomiwa <adetayoomotomiwa99@gmail.com>
2020-11-06 03:31:11 -0800

    Livescore app

---
+++ precache-manifest.f00d267cbd961ea1b1d9dc221935e5b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52cb03c8662c2e6c06e8e1fd5cdd8dd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16ff03d227c704eec463",
+    "url": "/static/css/main.e929e268.chunk.css"
+  },
+  {
+    "revision": "f6b363250cddbfecb46d",
+    "url": "/static/js/2.cb33ae00.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.cb33ae00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16ff03d227c704eec463",
+    "url": "/static/js/main.5134c436.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
